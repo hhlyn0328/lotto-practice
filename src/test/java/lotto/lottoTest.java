50afe3a33a09e78fd19f447cf3f28e0ed95680d5
eunhee.choi <eunhee.choi@kurlycorp.com>
2021-01-08 10:53:05 +0900

    test: test 작성

---
+++ src/test/java/lotto/lottoTest.java
@@ -0,0 +1,17 @@
+package lotto;
+
+import org.junit.jupiter.api.Test;
+
+public class lottoTest {
+    @Test
+    void lotto_1장_6개숫자_test() {
+        // 1장 구매시 6개의 숫자가 생성되었는지
+
+    }
+
+    @Test
+    void lotto_2장_6개숫자_test() {
+        // 2장 구매시 각 6개의 숫자가 생성되었는지
+    }
+
+}
---
+++ src/test/java/lotto/priceText.java
@@ -0,0 +1,16 @@
+package lotto;
+
+import org.junit.jupiter.api.Test;
+
+public class priceText {
+    @Test
+    void price_천원미만_test() {
+        //1000원 미만의 금이면 예외
+    }
+
+    @Test
+    void price_로또개수_test() {
+        //입력한 금액 /1000 의 몫만큼 로또개수가 일치하는지
+    }
+
+}
